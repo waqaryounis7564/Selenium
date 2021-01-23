4f2e0b3bb998bdc9cdd634903fd0457853a8f03b
Waqar <waqar.younas7564@gmail.com>
2021-01-23 10:21:59 +0500

    first commit

---
+++ src/main/java/com/example/seleniumdemo/Demo.java
@@ -0,0 +1,56 @@
+package com.example.seleniumdemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Demo {
+    public static void crawl(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--start-fullscreen");
+        WebDriver driver=new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.londonstockexchange.com/news-article/BGHS/Cancellation of euro shares held in treasury/14834659");
+        Screenshot screenshot;
+//        driver.switchTo().alert().accept();
+        screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+        try {
+            ImageIO.write(screenshot.getImage(), "jpg", new File("d://test.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//
+//        try {
+//            takeSnapShot(driver,"d://test.png") ;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(content);
+        driver.close();
+        driver.quit();
+
+    }
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile=new File(fileWithPath);
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    }
+
+
+
---
+++ src/main/java/com/example/seleniumdemo/SeleniumdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.seleniumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumdemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SeleniumdemoApplication.class, args);
+        Demo.crawl();
+    }
+
+}
