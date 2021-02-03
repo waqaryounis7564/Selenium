d0603b4aeda0b8c262052f33efa895a336b94605
Waqar <waqar.younas@2iqresearch.com>
2021-02-03 12:35:15 +0500

    clickEvent of accept cookies resolved

--- src/main/java/com/example/seleniumdemo/Demo.java
+++ src/main/java/com/example/seleniumdemo/Demo.java
@@ -5,27 +5,36 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 import javax.imageio.ImageIO;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
 public class Demo {
-    public static void crawl(){
+    public static void crawl() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--start-fullscreen");
-        WebDriver driver=new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriver driver = new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.get("https://www.londonstockexchange.com/news-article/BGHS/Cancellation of euro shares held in treasury/14834659");
         Screenshot screenshot;
+
 //        driver.switchTo().alert().accept();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//*[@id=\"ccc-notify-accept\"]")).click();
         screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
         try {
             ImageIO.write(screenshot.getImage(), "jpg", new File("d://test.jpg"));
@@ -43,14 +52,15 @@ public class Demo {
         driver.quit();
 
     }
-    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
-        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
-        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
-        File DestFile=new File(fileWithPath);
+
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(fileWithPath);
         FileUtils.copyFile(SrcFile, DestFile);
     }
 
-    }
+}
 
 
 
